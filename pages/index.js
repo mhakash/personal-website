b6b9cb8263e5987eca32b0800c072e2d88c6c278
mhakash <mhakash211998@gmail.com>
2020-08-20 14:29:33 +0600

    add name and links

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import {
+  IoLogoGithub as GithubLogo,
+  IoLogoFacebook as FacebookLogo,
+  IoLogoLinkedin as LinkedInLogo,
+} from "react-icons/io";
 
 export default function Home() {
   return (
@@ -11,56 +16,30 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className={styles.title} style={{fontSize:"3rem"}}>Mehdi Hassan Akash</h1>
+        <ul style={{ display: "flex", listStyle: "none" }}>
+          <li style={{ marginRight: "50px" }}>
+            <a href="https://github.com/mhakash" target="_">
+              <GithubLogo title="github" size={32}/>
+            </a>
+          </li>
+          <li style={{ marginRight: "50px" }}>
+            <a href="https://facebook.com/mhakash21" target="_">
+              <FacebookLogo title="facebook" size={32} />
+            </a>
+          </li>
+          <li style={{ marginRight: "50px" }}>
+            <a href="https://linkedin.com/in/mhakash" target="_">
+              <LinkedInLogo title="linkedin" size={32}/>
+            </a>
+          </li>
+        </ul>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        Powered by{" "}
+        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
       </footer>
     </div>
-  )
+  );
 }
